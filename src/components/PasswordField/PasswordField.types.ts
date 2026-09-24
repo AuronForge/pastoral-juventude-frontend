ade5fccd7b642e8a4fc46a0395c165e74afecdf5
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2026-09-24 09:52:20 -0300

    feat: reveal password in PasswordField

--- src/components/PasswordField/PasswordField.types.ts
+++ src/components/PasswordField/PasswordField.types.ts
@@ -1,4 +1,12 @@
 import type { TextFieldProps } from "../TextField";
 
 /** Campo de texto especializado para informações sigilosas. */
-export type PasswordFieldProps = Omit<TextFieldProps, "type">;
+export type PasswordFieldProps = Omit<
+  TextFieldProps,
+  "trailingIcon" | "type"
+> & {
+  /** Nome acessível da ação exibida quando a senha está visível. */
+  hidePasswordLabel?: string;
+  /** Nome acessível da ação exibida quando a senha está oculta. */
+  showPasswordLabel?: string;
+};
